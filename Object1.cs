8d77792f167df644c14a65584f71da31699eb7c3
TomasRampas <rampas.tomas@gmail.com>
2017-03-21 10:43:17 +0100

    Game scripts

---
+++ Object1.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object1 : MonoBehaviour {
+
+    [SerializeField] private float objectSpeed = 1;
+    [SerializeField] private float resetPosition = -21.65f;
+    [SerializeField] private float startPosition = 79.67f;
+
+    void Start () {
+		
+	}
+	
+	protected virtual void Update ()
+    {
+        if (!GameManager.instance.GameOver)
+        {
+            transform.Translate(Vector3.left * (objectSpeed * Time.deltaTime));
+            if (transform.localPosition.x <= resetPosition)
+            {
+                Vector3 newPos = new Vector3(startPosition, transform.position.y, transform.position.z);
+                transform.position = newPos;
+            }
+        }
+	}
+}
